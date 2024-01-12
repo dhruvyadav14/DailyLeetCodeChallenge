32523c968c034fa5f6b7f615afa9be108d6c2b46
Dhruv Yadav <yadav.dhruv@gmail.com>
2024-01-11 22:28:11 -0500

    very basic string sum

---
+++ January/Jan12-LC1704.py
@@ -0,0 +1,4 @@
+class Solution:
+    def halvesAreAlike(self, s: str) -> bool:
+        vowelSet = set(['a','e','i','o','u'])
+        return sum(1 for char in s[:len(s)//2] if char.lower() in vowelSet) == sum(1 for char in s[len(s)//2:] if char.lower() in vowelSet)
