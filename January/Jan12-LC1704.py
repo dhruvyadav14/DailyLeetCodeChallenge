790c90cd761975821f0fc79b6814b1f6f380d3d0
Dhruv Yadav <yadav.dhruv@gmail.com>
2024-01-15 20:17:01 -0500

    random changes

--- January/Jan12-LC1704.py
+++ January/Jan12-LC1704.py
@@ -1,4 +1,3 @@
-class Solution:
-    def halvesAreAlike(self, s: str) -> bool:
-        vowelSet = set(['a','e','i','o','u'])
-        return sum(1 for char in s[:len(s)//2] if char.lower() in vowelSet) == sum(1 for char in s[len(s)//2:] if char.lower() in vowelSet)
+def halvesAreAlike(self, s: str) -> bool:
+    vowelSet = set(['a','e','i','o','u'])
+    return sum(1 for char in s[:len(s)//2] if char.lower() in vowelSet) == sum(1 for char in s[len(s)//2:] if char.lower() in vowelSet)
--- January/Jan13-LC1347.py
+++ January/Jan13-LC1347.py
@@ -1,13 +1,6 @@
 from collections import Counter 
 class Solution:
-    def minSteps(self, s: str, t: str) -> int:
-        # first thought is to create a map counter for each letter
-        """
-        {c:1, t:1, e:3, l:1, o:1, d:1}
-        {c:2, t:1, p:1, r:1, a:1, i:1, e:1}
-        """
-
-        
+    def minSteps(self, s: str, t: str) -> int:     
         cnt, steps = Counter(s), 0
         for c in t:
             if cnt[c] > 0:
