9a8e0428b9a9fb836f136815c587c598967b476a
Dhruv Yadav <yadav.dhruv@gmail.com>
2024-01-16 10:36:08 -0500

    adding jan1

---
+++ January/Jan1-LC455.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+
+        total=0
+        i,j=0,0
+
+        while i<len(g) and j<len(s):
+            if g[i] <= s[j]:
+                total+=1
+                i+=1
+            j+=1
+
+        return total
