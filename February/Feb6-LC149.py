27cc01b9af0f60bee9b4e3b105175f9e6d9ddb0c
Dhruv Yadav <dhruvyadav@Dhruvs-Air.fios-router.home>
2024-02-07 15:00:40 -0500

    first commit in a couple weeks, yikes

---
+++ February/Feb6-LC149.py
@@ -0,0 +1,8 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        sortedMap = defaultdict(list)
+        for word in strs:
+            sortedWord = ''.join(sorted(word))
+            sortedMap[sortedWord].append(word)
+
+        return [values for values in sortedMap.values()]
